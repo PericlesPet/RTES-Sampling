41b4e6578a514dad508cc64df9947dc2239644c8
PericlesPet <YOUR@EMAIL.com>
2019-05-05 13:33:30 +0300

    First edition, just timestamps

---
+++ timestamps.c
@@ -0,0 +1,54 @@
+// #include <chrono>
+// #include <thread>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+void sleep_ms(int milliseconds);
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  // INTRO
+  if (argc =! 3){
+    printf("incorrect args --- total_time, dt \n");
+  }
+  float total_time = atoi(argv[1]);
+  float time_dt = atof(argv[2]);
+  float time_dt_ms = time_dt * 1000;
+
+  printf("Total Time = %f \nTime_dt = %f \nTime_dt_ms=%f\n\n",total_time,time_dt,time_dt_ms);
+  // BEGIN
+  struct timeval startwtime;
+  struct timeval endwtime;
+  float real_dt;
+  float real_total_time=0;
+
+  // TIME LOOP
+  for(float i=0;i<total_time;i+=time_dt){
+
+
+    gettimeofday(&startwtime,NULL);
+    sleep_ms(time_dt_ms);
+    // std::this_thread::sleep_for(std::chrono::milliseconds());
+    gettimeofday(&endwtime,NULL);
+
+    // printf("%ld -- %ld -- %f -- %f \n",tv2.tv_usec,tv1.tv_usec,(float)((tv2.tv_usec)/1000000.0f),(tv1.tv_sec+tv1.tv_usec/1000000.0f));
+    real_dt = (float)((endwtime.tv_usec - startwtime.tv_usec)/1.0e6
+  + endwtime.tv_sec - startwtime.tv_sec);
+    printf("time_dt = %f ---- real_dt = %f ||--|| virtual_time = %f ---- real_time = %f \n",time_dt,real_dt, i, real_total_time);
+    real_total_time += real_dt;
+  }
+
+  return 0;
+}
+
+
+
+//HELPERS
+void sleep_ms(int milliseconds){
+struct timespec ts;
+    ts.tv_sec = milliseconds / 1000;
+    ts.tv_nsec = (milliseconds % 1000) * 1000000;
+    nanosleep(&ts, NULL);
+}
